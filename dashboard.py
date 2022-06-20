761c70aa86230f9c0f69d7fda386acf06845bf58
Andrew Semchism <andrew0187@gmail.com>
2022-06-20 18:42:22 -0400

    Initial commit

---
+++ dashboard.py
@@ -0,0 +1,72 @@
+from RPLCD import i2c
+from time import sleep
+import requests, json
+import datetime
+from config import openWeatherAPI
+
+# Set up LCD Display
+cols = 20
+rows = 4
+charmap = 'A00'
+i2c_expander = 'PCF8574'
+address = 0x27
+port = 1
+lcd = i2c.CharLCD(i2c_expander=i2c_expander, address=address, port=port, charmap=charmap, cols=cols, rows=rows)
+
+# API Call Globals
+lastUpdate = datetime.datetime(2000, 1, 1)
+weatherData = {}
+
+def displayWeather():
+  global weatherData
+  if weatherData["cod"] != "404":
+    current_temperature = weatherData["main"]["temp"] - 273.15
+    high_temperature = weatherData["main"]["temp_max"] - 273.15
+    low_temperature = weatherData["main"]["temp_min"] - 273.15
+    lcd.clear()
+    lcd.write_string('----TEMPERATURE-----')
+    lcd.write_string('    Now: ' + str(current_temperature) + 'C')
+    lcd.cursor_pos = (2, 0)
+    lcd.write_string('    High: ' + str(high_temperature) + 'C')
+    lcd.cursor_pos = (3, 0)
+    lcd.write_string('    Low: ' + str(low_temperature) + 'C')
+    lcd.crlf()
+  else:
+    lcd.clear()
+    lcd.write_string('-------WEATHER------')
+    lcd.write_string('ERROR: No data')
+    lcd.crlf()
+  sleep(6)
+
+def getWeatherData():
+  global weatherData
+  response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=42.956019&lon=-81.293685&appid=" + openWeatherAPI)
+  weatherData = response.json()
+
+def displayTime():
+  currentTime = datetime.datetime.now()
+  displayTime = currentTime.strftime('       %I:%M')
+  displayDate = currentTime.strftime(' %a, %b %d, %Y')
+  lcd.clear()
+  lcd.write_string('--------TIME--------')
+  lcd.write_string(displayTime)
+  lcd.cursor_pos = (3, 0)
+  lcd.write_string(displayDate)
+  lcd.crlf()
+  sleep(6)
+
+while (True):
+  # Update Data
+  currentTime = datetime.datetime.now()
+  elapsedTime = currentTime - lastUpdate
+  elapsedTimeSeconds = elapsedTime.total_seconds()
+  if (elapsedTimeSeconds > 60*5):
+    lastUpdate = currentTime
+    print("Updating Data")
+    getWeatherData()
+
+
+  displayTime()
+  displayWeather()
+  
+
